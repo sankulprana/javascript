6cd3de619b58c5bdb3deed82557806c02d980ada
Sankulp <sankulp99@gmail.com>
2026-07-03 19:27:11 +0000

    update

--- 01_basics/1st.js
+++ 01_basics/1st.js
@@ -1,1 +1,10 @@
-console.log("hello")
+// console.log("hello")
+const val= 144563
+let ans = 4
+let account_email="sankulp99@gamil.com"
+
+let accounstate;
+// val=568
+console.log(ans)
+console.log(val)
+console.table([val,ans,account_email,accounstate])
