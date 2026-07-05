359b5c2cd647e45571c066dcb39383702ae707f3
Sankulp <sankulp99@gmail.com>
2026-07-05 12:23:35 +0000

    converstion

--- 01_basics/1st.js
+++ 01_basics/1st.js
@@ -9,4 +9,27 @@ console.log(ans)
 console.log(val)
 console.table([val,ans,account_email,accounstate])
 
+
 // alert(3+5) we are using node not browser
+
+//number
+//bigint
+//boolean
+//string => "  "
+//NULL type;
+//undefined
+let j;
+console.log(typeof(j))
+
+
+let score = "56a"
+console.log(typeof(score))
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+
+let isloggedin = "hello"
+let is =Boolean(isloggedin)
+console.log(is);
+ console.log(typeof(is))
